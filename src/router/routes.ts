55a1a5b4fd77be4add78f9fc1af58a7a24ae46f0
Jaswanth Sai Sattenapalli <jaswanthsaisattenapalli@gmail.com>
2025-08-16 03:42:00 +0530

    add home redirection

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,6 +3,12 @@ import type { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
+    redirect: {
+      path: '/cards',
+    },
+  },
+  {
+    path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
