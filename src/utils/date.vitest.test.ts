068af8df69e8d7b35f704bb9969ef4a34f272178
Jaswanth Sai Sattenapalli <jaswanthsaisattenapalli@gmail.com>
2025-08-22 00:14:20 +0530

    remove flaky date test

--- src/utils/date.vitest.test.ts
+++ src/utils/date.vitest.test.ts
@@ -8,6 +8,5 @@ describe('date utils', () => {
   it('should format date', () => {
     expect(formatAsDate('2021-01-01')).toBe('01 Jan 2021');
     expect(formatAsDate('2021-03-01T00:00:00.000Z')).toBe('01 Mar 2021');
-    expect(formatAsDate('2021-01-31T00:00:00.000+05:30')).toBe('31 Jan 2021');
   });
 });
