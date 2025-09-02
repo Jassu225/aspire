37d664ec818b67f2437230fdf949bad21a39f054
Jaswanth Sai Sattenapalli <jaswanthsaisattenapalli@gmail.com>
2025-09-02 19:55:48 +0530

    use emit mockery events

--- src/services/mockery/mockery.ts
+++ src/services/mockery/mockery.ts
@@ -1,4 +1,9 @@
 import { findMockAndExec } from 'src/services/mock-backend/back-end';
+import type {
+  MockeryRequestEventDetail,
+  MockeryResponseEventDetail,
+} from './mockery-event-emitter';
+import mockeryEventEmitter, { MockeryEvents } from './mockery-event-emitter';
 
 const setupMockery = (): (() => void) => {
   console.log(`--------- Mockery Enabled -------`);
@@ -8,8 +13,13 @@ const setupMockery = (): (() => void) => {
   globalThis.fetch = async (input: RequestInfo | URL, init?: RequestInit): Promise<Response> => {
     if (typeof input === 'string') {
       try {
-        const resPromise = findMockAndExec(input, JSON.parse((init?.body as string) || '{}'));
+        const request = JSON.parse((init?.body as string) || '{}');
+        const resPromise = findMockAndExec(input, request);
         if (resPromise instanceof Promise) {
+          mockeryEventEmitter.emit<MockeryRequestEventDetail<unknown>>(MockeryEvents.ON_REQUEST, {
+            request,
+            url: input,
+          });
           const res = await resPromise;
           const response = new Response(JSON.stringify(res), {
             status: 200,
@@ -18,6 +28,11 @@ const setupMockery = (): (() => void) => {
               'Content-Type': 'application/json',
             },
           });
+
+          mockeryEventEmitter.emit<MockeryResponseEventDetail<unknown, object>>(
+            MockeryEvents.ON_RESPONSE,
+            { request, response: res, url: input },
+          );
           return response;
         }
       } catch (e) {
